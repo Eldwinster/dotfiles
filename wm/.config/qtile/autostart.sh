bd2b1160c9c512c7e1e3e3d06bcea3958b10e05f
Eldwinster <yann.arby@gmail.com>
2023-09-15 00:31:19 +0200

    qtile: updated some obsolete or unworking stuff

--- wm/.config/qtile/autostart.sh
+++ wm/.config/qtile/autostart.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+# mons --primary LVDS2 -e top || mons --primary LVDS1 -e top
 picom &
 xbanish &
-setxkbmap us qwerty-fr &
-mons -a -x "$HOME/.bin/mons.sh" & disown
+# mons -a -x "$HOME/.bin/mons.sh" & disown
 emacs --daemon &
 wallpaper.sh random auto 120m &
+setxkbmap us qwerty-fr &
+# mons --pr
